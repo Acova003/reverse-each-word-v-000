48770955143352dfe35e0768abda96aef0c007ba
Amee Covarrubias <ameecova@yahoo.com>
2018-07-21 05:50:29 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,6 @@
 def reverse_each_word(sentence)
   sentence = sentence.split(" ")
-  #sentence = sentence.map(&:reverse!)
-  #sentence.join(" ")
-  
-  #each_rev = sentence
   sentence.each {|word| word.reverse}.join(" ")
-  #end
-  sentence.collect {|word| word.reverse}.join(" ")
-  #end
 
-  
-  
-  
-    
+  sentence.collect {|word| word.reverse}.join(" ")
 end
